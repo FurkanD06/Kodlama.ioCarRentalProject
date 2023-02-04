c3d629177be5f07c223c49d427b3c8a6225bc5f7
Furkan Demir <fd2366131@gmail.com>
2023-02-04 21:12:05 +0300

    Araç resimleri eklendi

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+RentalManager rentalManager = new RentalManager(new EfRentalDal());
+//rentalManager.Add(new Entities.Concrete.Rental { CustomerId = 1004, CarId = 4006, RentDate = DateTime.Now, ReturnDate = DateTime.Now.AddDays(2) });
